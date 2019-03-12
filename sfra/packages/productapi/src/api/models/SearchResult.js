2315b60f0d56782839634a041d00796f1550d314
Jason Cooper <coopaq@yahoo.com>
2019-03-12 14:14:36 -0400

    use npm.lwcjs.org

--- sfra/packages/productapi/src/api/models/SearchResult.js
+++ sfra/packages/productapi/src/api/models/SearchResult.js
@@ -5,7 +5,7 @@ import SearchResultProduct from "./SearchResultProduct";
 class SearchResult {
     constructor(searchResult, filterParams) {
         this.count = searchResult.count;
-        this.productHits = searchResult.hits.map((product) => new SearchResultProduct(product));
+        this.productHits = searchResult['hits'] && searchResult['hits'].length ? searchResult.hits.map((product) => new SearchResultProduct(product)) : [];
         this.currentFilters = filterParams ? filterParams : null;
         this.refinements = searchResult.refinements.map((refinement) => {
             return {
