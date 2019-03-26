ad6a9c7195a88bcfadb2ca860913bf4724bc9ba6
Jason Cooper <coopaq@yahoo.com>
2019-03-26 16:30:10 -0400

    bump alpha-9 to alpha-10

--- publish-modules.sh
+++ publish-modules.sh
@@ -3,10 +3,41 @@
 #echo Build and Publish all
 cd sfcc
 lerna bootstrap --hoist
-npm run pub
-cd ../sfra
-lerna bootstrap --hoist
+
+# CORE
+cd packages/core
+npm run clean
 npm run pub
 
-cd ..
+# LOGGER
+cd ../logger
+npm run clean
+npm run pub
+
+# APICONFIG
+cd ../apiconfig
+npm run clean
+npm run pub
+
+# CORE-GRAPHQL
+cd ../core-graphql
+npm run clean
+npm run pub
+
+# SFRA
+cd ../../../sfra
+lerna bootstrap --hoist
+
+# PRODUCTAPI
+cd packages/productapi
+npm run clean
+npm run pub
+
+# CONTENTAPI
+cd ../contentapi
+npm run clean
+npm run pub
+
+
+cd ../../../
 
