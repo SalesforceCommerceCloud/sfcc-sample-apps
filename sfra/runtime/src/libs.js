889e024cbd98bdbefa78bb1b8c29592026facbb8
Jason Cooper <coopaq@yahoo.com>
2019-04-01 15:01:38 -0400

    add passport to libs

--- sfra/runtime/src/libs.js
+++ sfra/runtime/src/libs.js
@@ -1,10 +1,13 @@
 import gql from 'graphql-tag';
 import ApolloClient from "apollo-boost";
 
+import passport from 'passport';
+
 // EXPOSE ApolloClient as a global.
 // Since lwc rollup compiler cannot handle graphql *.mjs files
 window.ApolloClient = ApolloClient;
 window.gql = gql;
+window.passport = passport;
 
 function test() {
     console.log('ApolloClient provided by libs.js');
