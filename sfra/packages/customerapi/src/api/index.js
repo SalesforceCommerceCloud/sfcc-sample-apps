179b12dc79b5312b0ed8bb54dd5201a56b9734af
Syed Shehroz Hussain (Ozzy) <s.hussain@salesforce.com>
2019-04-01 15:18:20 -0400

    New: Added customer API BFF

---
+++ sfra/packages/customerapi/src/api/index.js
@@ -0,0 +1,4 @@
+// Customer
+export {typeDef as customerTypeDef} from './schema/customerTypeDef';
+export {resolver as customerResolver} from './schema/customerResolvers';
+export { model as customerDataSource } from './models/customer';
--- sfra/packages/productapi/src/api/index.js
+++ sfra/packages/productapi/src/api/index.js
@@ -5,8 +5,3 @@ export {resolver as productDetailsResolver} from './schema/productDetailsResolve
 // Product Search
 export {typeDef as productSearchTypeDef} from './schema/productSearchTypeDef';
 export {resolver as productSearchResolver} from './schema/productSearchResolvers';
-
-// Customer
-export {typeDef as customerTypeDef} from './schema/customerTypeDef';
-export {resolver as customerResolver} from './schema/customerResolvers';
-export { model as customerDataSource } from './models/customer';
