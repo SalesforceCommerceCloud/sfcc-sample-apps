b587dcc782dc930dceafa9ee72d41ed1021cd221
Syed Shehroz Hussain (Ozzy) <s.hussain@salesforce.com>
2019-04-12 13:39:44 -0400

    create cart and delete cart

--- sfra/packages/cartapi/src/api/models/cart.js
+++ sfra/packages/cartapi/src/api/models/cart.js
@@ -7,45 +7,59 @@ class Cart extends RESTDataSource {
         super();
         this.baseURL = config.COMMERCE_BASE_URL;
         this.clientId = config.COMMERCE_APP_API_CLIENT_ID;
+        this.authToken = '';
     }
 
-    // willSendRequest(request) {
-    //     request.headers.set("Authorization", this.context.token);
-    // }
+    willSendRequest(request) {
+        request.headers.set("Authorization", this.authToken);
+    }
 
-    // async didReceiveResponse(response) {
+    async didReceiveResponse(response) {
+        return response;
+    }
 
-    //     // TODO: return just the response here, move the logic to get the authorization header in the getBearerToken() function.
-    //     const authToken = response.headers.get('Authorization');
-    //     const body = await response.json();
-    //     return { body, authentication: authToken };
-    // }
+    async getBearerToken() {
+        const body = {
+            "type": "guest"
+        };
+        const response = await this.post(`customers/auth?client_id=${this.clientId}`, body);
+        this.authToken = response.headers.get('Authorization');
+        return await response.json();
+    }
 
     async getCart(cartId) {
-        return {
-            cartId: "123",
-            products: [{
-                id: "product",
-                name: "product",
-                price: 22.40,
-                currency: "USD",
-                page_description: "something",
-                long_description: "something",
-                short_description: "something",
-                primary_category_id: "something",
-                image: "something",
-                images: [{
-                    title: "something",
-                    alt: "something",
-                    link: "something"
-                }]
-            }]
-        }
+        // return {
+        //     cartId: "123",
+        //     products: [{
+        //         id: "product",
+        //         name: "product",
+        //         price: 22.40,
+        //         currency: "USD",
+        //         page_description: "something",
+        //         long_description: "something",
+        //         short_description: "something",
+        //         primary_category_id: "something",
+        //         image: "something",
+        //         images: [{
+        //             title: "something",
+        //             alt: "something",
+        //             link: "something"
+        //         }]
+        //     }]
+        // }
     }
 
     async createCart() {
-        return {
-            cartId: "123",
+        const bearerToken = await this.getBearerToken();
+        const headers = {
+            'Authorization': this.authToken
+        }
+        const cartObj = await this.post('baskets', {}, headers).then((response) => {
+            return response.json();
+        });
+        console.log(cartObj);
+        const cart = {
+            cartId: cartObj.basket_id,
             products: [{
                 id: "product",
                 name: "product",
@@ -62,15 +76,17 @@ class Cart extends RESTDataSource {
                     link: "something"
                 }]
             }]
-        }
+        };
+        return cart;
     }
 
     async deleteCart(cartId) {
+        const deleteCart = await this.delete(`baskets/${cartId}`);
         return `cart with cartID: ${cartId} successfully deleted`;
     }
 
     async addToCart(productId) {
-        return `product with productId: ${productId} successfully added to cart`;
+        // return `product with productId: ${productId} successfully added to cart`;
     }
 
 }
