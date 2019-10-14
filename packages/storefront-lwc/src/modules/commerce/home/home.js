3c65dc95fcf6dc96b599c2049c431fb8373b1956
Zi Sardone <zsardone@salesforce.com>
2019-10-14 10:11:32 -0400

    New : SampleAHomepage (W-6740678)

--- packages/storefront-lwc/src/modules/commerce/home/home.js
+++ packages/storefront-lwc/src/modules/commerce/home/home.js
@@ -16,53 +16,6 @@ class Home extends LightningElement {
         super();
     }
 
-    connectedCallback() {
-
-        try {
-            // TODO: read from api config
-            var client = new window.ApolloClient({
-                uri: window.apiconfig.COMMERCE_API_PATH || "/graphql"
-            });
-
-            return client.query({
-                query: window.gql`
-                        {
-                            content(contentIds: ["home-main", "home-categories"]) {
-                            body
-                            }
-                        }
-                     `
-            }).then(result => {
-                console.log(result);
-                const container = this.template.querySelector('.container');
-                container.innerHTML += result.data.content[0].body;
-                container.innerHTML += result.data.content[1].body
-                return result;
-            }).catch((error) => {
-                console.log(error);
-                return {
-                    error
-                };
-            });
-        } catch (e) {
-            return console.log('Error in Home connectedCallback():',  e);
-        }
-
-        // fetchContents( [ 'home-main', 'home-categories' ] )
-        //     .then( json => {
-        //         console.log( json );
-        //         if (json.data && json.data.length ) {
-        //             json.data.forEach(data => {
-        //                 const container = this.template.querySelector('.container');
-        //                 container.innerHTML += data.c_body;
-        //             })
-        //         }
-        //     } )
-        //     .catch( e => {   // eslint-disable-line no-unused-vars
-        //         console.log( e )
-        //     } );
-    }
-
     renderedCallback() {
     }
 }
