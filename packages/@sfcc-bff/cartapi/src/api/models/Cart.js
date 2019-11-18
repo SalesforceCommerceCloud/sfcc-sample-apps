d6ff68e70f9e9298b69b51d9a2c2ef406d3f8196
Zi Sardone <zsardone@salesforce.com>
2019-11-18 14:06:22 -0500

    new: refactor continued on addToCart, deleteFromCart

--- packages/@sfcc-bff/cartapi/src/api/models/Cart.js
+++ packages/@sfcc-bff/cartapi/src/api/models/Cart.js
@@ -1,117 +1,19 @@
 'use strict';
 
-// const { RESTDataSource } = require('apollo-datasource-rest');
-
 class Cart {
     constructor(apiCart) {
-        var obj = {
-            productId: '123',
-            itemId: '234'
-        };
-        console.log('apiCart is ', apiCart.basket_id);
-        this.cartId = apiCart.basket_id;
-        this.products = [obj];
+        this.authToken = '';
+        this.cartId = apiCart.basket_id ? apiCart.basket_id: '';
+        this.products = apiCart.product_items ? apiCart.product_items.map((product) => {
+            return {
+                productId: product.product_id,
+                productName : product.product_name,
+                quantity: product.quantity,
+                itemId: product.item_id
+            };
+        }) : []
         Object.assign(this, apiCart);
     }
 }
-
-// willSendRequest(request) {
-//     request.headers.set("Authorization", this.authToken);
-// }
-
-// async didReceiveResponse(response) {
-//     return response;
-// }
-
-// TODO: convert current productapi implementation to datasource method (like this one, customerapi and loginapi) and then re-use that for products here
-// TODO: move logic of handling bearer token out into its own module and use session management there
-// TODO: proper error handling
-
-
-
-// async getCart(cartId) {
-//     this.memoizedResults.clear();
-//     const headers = {
-//         "Authorization": this.authToken
-//     };
-//     const cartObj = await this.get(`baskets/${cartId}`).then((response) => {
-//         return response.json();
-//     });
-//     const cart = {
-//         cartId: cartObj.basket_id,
-//         products: cartObj.product_items ? cartObj.product_items.map((product) => {
-//             return {
-//                 productId: product.product_id,
-//                 itemId: product.item_id
-//             };
-//         }) : []
-//     };
-//     return cart;
-// }
-
-// async createCart() {
-//     const bearerToken = await this.getBearerToken();
-//     const headers = {
-//         "Authorization": this.authToken
-//     };
-//     const cartObj = await this.post('baskets', {}, headers).then((response) => {
-//         return response.json();
-//     });
-//     const cart = {
-//         cartId: cartObj.basket_id
-//     };
-//     return cart;
-// }
-
-// async deleteCart(cartId) {
-//     const deleteCart = await this.delete(`baskets/${cartId}`);
-//     return `cart with cartID: ${cartId} successfully deleted`;
-// }
-
-// TODO: convert current productapi implementation to datasource method (like this one, customerapi and loginapi) and then re-use that for products here
-
-// async addToCart(cartId, productId) {
-//     const headers = {
-//         "Authorization": this.authToken
-//     };
-//     const body = [{
-//         "product_id": productId,
-//         "quantity": 1.00
-//     }];
-//     const cartObj = await this.post(`baskets/${cartId}/items`, body).then((response) => {
-//         return response.json();
-//     });
-//     const cart = {
-//         cartId: cartObj.basket_id,
-//         products: cartObj.product_items ? cartObj.product_items.map((product) => {
-//             return {
-//                 productId: product.product_id,
-//                 itemId: product.item_id
-//             };
-//         }) : []
-//     };
-//     return cart;
-// }
-
-// async deleteFromCart(cartId, itemId) {
-//     const headers = {
-//         "Authorization": this.authToken
-//     };
-//     const cartObj = await this.delete(`baskets/${cartId}/items/${itemId}`).then((response) => {
-//         return response.json();
-//     });
-//     const cart = {
-//         cartId: cartObj.basket_id,
-//         products: cartObj.product_items ? cartObj.product_items.map((product) => {
-//             return {
-//                 productId: product.product_id,
-//                 itemId: product.item_id
-//             };
-//         }) : []
-//     };
-//     return cart;
-// }
-// }
-
 
 export default Cart;
--- packages/@sfcc-bff/cartapi/src/api/schema/cartResolvers.js
+++ packages/@sfcc-bff/cartapi/src/api/schema/cartResolvers.js
@@ -6,36 +6,79 @@ const getBearerToken = async(config) => {
     const body = {
         "type": "guest"
     };
-    const getToken_URL = `${config.COMMERCE_BASE_URL}/customers/auth?client_id=${config.COMMERCE_APP_API_CLIENT_ID}`;
-    const response = await fetch(getToken_URL, {
+    const getTokenUrl = `${config.COMMERCE_BASE_URL}/customers/auth?client_id=${config.COMMERCE_APP_API_CLIENT_ID}`;
+    const response = await fetch(getTokenUrl, {
         method: 'post',
         body:    JSON.stringify(body),
         headers: { 'Content-Type': 'application/json' },
     });
-    return response.headers.get('Authorization');
+    Cart.authToken = response.headers.get('Authorization');
+    return await response.json();
 }
 
-const createBasket = async (config) => {
-    const bearerToken = await getBearerToken(config);
-    const createBasket_URL = `${config.COMMERCE_BASE_URL}/baskets`;
-    return await fetch(createBasket_URL, {
+const createCart = async (config) => {
+    await getBearerToken(config);
+    const createBasketUrl = `${config.COMMERCE_BASE_URL}/baskets`;
+    return await fetch(createBasketUrl, {
         method: 'post',
-        headers: { 'Content-Type': 'application/json', 'Authorization': bearerToken },
+        headers: { 'Content-Type': 'application/json', 'Authorization': Cart.authToken }
+    }).then(res => res.json());
+}
+
+const addToCart = async (cartId, productId, quantity, config) => {
+   // TODO : check if basket exists, if not create a new one
+   // If yes, check to see if items exsits, if yes, increment the quantity;
+   // validate if the item is available before add to Cart
+    const body = [{
+        "product_id": productId,
+        "quantity": quantity
+    }];
+    const addToCartUrl = `${config.COMMERCE_BASE_URL}/baskets/${cartId}/items`;
+    return await fetch(addToCartUrl, {
+        method: 'post',
+        body: JSON.stringify(body),
+        headers: { 'Content-Type': 'application/json', 'Authorization': Cart.authToken }
+    }).then(res => res.json());
+}
+
+const getCart = async (cartId, config) => {
+    // Cart.memorizedResults.clear();
+    const getCartUrl = `${config.COMMERCE_BASE_URL}/baskets/${cartId}`;
+    return await fetch(getCartUrl, {
+        method: 'get',
+        headers: { 'Content-Type': 'application/json', 'Authorization': Cart.authToken }
+    }).then(res => res.json());
+
+}
+
+const deleteFromCart = async (cartId, productId, config) => {
+    const deleteFromCartUrl = `${config.COMMERCE_BASE_URL}/baskets/${cartId}/items/${productId}`;
+    return await fetch(deleteFromCartUrl, {
+        method: 'delete',
+        headers: { 'Content-Type': 'application/json', 'Authorization': Cart.authToken }
     }).then(res => res.json());
 }
 
 export const resolver = (config) => {
     return {
         Query: {
-            getCart: (_, { cartId }) => {
-                return dataSources.cart.getCart(cartId);
+            getCart: async (_, { cartId }) => {
+                const apiCart = await getCart (cartId, config);
+                return new Cart(apiCart);
             }
         },
         Mutation: {
             createCart: async (_, {}) => {
-                const myCart = await createBasket(config);
-                console.log('hahaha I got the myCart is ', myCart);
-                return new Cart(myCart);
+                const apiCart = await createCart(config);
+                return new Cart(apiCart);
+            },
+            addToCart: async (_, {cartId, productId, quantity}) => {
+                const apiCart = await addToCart (cartId, productId, quantity, config);
+                return new Cart(apiCart);
+            },
+            deleteFromCart: async (_, { cartId, productId}) => {
+                const apiCart = await deleteFromCart (cartId, productId, config);
+                return new Cart(apiCart);
             }
         }
     }
