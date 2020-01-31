f28e6aca773da5a54c8f33f8af321e48cc88c5f5
Bret Little <bret.little@gmail.com>
2020-01-31 10:50:00 -0500

    Cleanup the searchsort page to properly use events

--- packages/storefront-lwc/src/modules/commerce/searchsort/searchsort.js
+++ packages/storefront-lwc/src/modules/commerce/searchsort/searchsort.js
@@ -4,7 +4,7 @@
     SPDX-License-Identifier: BSD-3-Clause
     For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
 */
-import { LightningElement, track } from 'lwc'
+import { LightningElement } from 'lwc'
 
 export default class SearchSort extends LightningElement {
     constructor() {
@@ -12,7 +12,7 @@ export default class SearchSort extends LightningElement {
         this.sortRule = this.sortOptions[0];
     }
 
-    @track sortOptions = [
+    sortOptions = [
         {id: 'best-matches', label: 'Best Matches'},
         {id: 'price-low-to-high', label: 'Price Low To High'},
         {id: 'price-high-to-low', label: 'Price High to Low'},
@@ -22,42 +22,22 @@ export default class SearchSort extends LightningElement {
         {id: 'top-sellers', label: 'Top Sellers'}
     ];
 
-    newSortRule = (event) => {
-        const newSortRule = event.target.value;
-        this.updateSortOptions(newSortRule);
+    get viewSortOptions() {
+        return this.sortOptions.map(sortOption => ({
+            ...sortOption,
+            selected: this.sortRule.id === sortOption.id
+        }))
     }
 
-    updateSortOptions(newSortRuleId) {
-        this.sortRule = this.sortOptions[0];
-        this.sortOptions.forEach(option => {
-            if (option.id !== newSortRuleId && option.selected) {
-                option.selected = null;
-                delete option.selected;
-            } else if (option.id === newSortRuleId) {
-                option.selected = 'selected';
-                this.sortRule = option;
-            }
-        });
-
-        const event = new CustomEvent('update-sort', {
+    newSortRule = (event) => {
+        const newSortRuleId = event.target.value;
+        this.sortRule = this.sortOptions.find(sortOption => sortOption.id === newSortRuleId);
+        this.dispatchEvent(new CustomEvent('update-sort', {
+            bubbles: true,
+            composed: true,
             detail: {
                 sortRule: this.sortRule
             }
-        });
-
-        window.dispatchEvent(event);
-    }
-    
-    renderedCallback() {
-        // TODO: ugh. why is LWC stripping 'option[selected]' attribute?
-        setTimeout(() => {
-            const sortSelect = this.template.querySelector('select[name=sort-order]');
-            if (sortSelect && sortSelect[0]) {
-                const option = sortSelect.querySelector(`option[class=${this.sortRule.id}]`);
-                if (option) {
-                    option.setAttribute('selected', 'selected');
-                }
-            }
-        })
+        }));
     }
 }
