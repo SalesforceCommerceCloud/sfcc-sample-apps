4f0bc13203f0fac8c9306a16504d26b69d491ca5
Evan Chessman <echessman@salesforce.com>
2020-02-03 08:59:18 -0500

    Chore: clean up header component

--- packages/storefront-lwc/src/modules/commerce/header/header.js
+++ packages/storefront-lwc/src/modules/commerce/header/header.js
@@ -4,18 +4,6 @@
     SPDX-License-Identifier: BSD-3-Clause
     For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
 */
-import { LightningElement, api, track } from 'lwc'
+import { LightningElement} from 'lwc'
 
-export default class CommerceHeader extends LightningElement {
-
-    @track logo;
-    @api test;
-
-    constructor() {
-        super();
-        this.logo = '/assets/images/logo.svg';
-    }
-
-    renderedCallback() {
-    }
-}
+export default class CommerceHeader extends LightningElement {}
