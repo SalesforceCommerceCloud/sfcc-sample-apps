87f75294408743671d8224a47f079108b577d0f5
Bret Little <blittle@salesforce.com>
2020-02-26 12:34:56 -0500

    Switch to interfaces

--- packages/@sfcc-core/core/src/types.ts
+++ packages/@sfcc-core/core/src/types.ts
@@ -1,14 +1,14 @@
-export type ApiConfig = {
+export interface ApiConfig {
     config: { [key: string]: string };
-};
-export type Logger = {
-    setLevel(level: LogLevel): void;
-    log(...args: Array<string>): void;
-    info(...args: Array<string>): void;
-    debug(...args: Array<string>): void;
-    warn(...args: Array<string>): void;
-    error(...args: Array<string>): void;
-};
+}
+export interface Logger {
+    setLevel: (level: LogLevel) => void;
+    log: (...args: Array<string>) => void;
+    info: (...args: Array<string>) => void;
+    debug: (...args: Array<string>) => void;
+    warn: (...args: Array<string>) => void;
+    error: (...args: Array<string>) => void;
+}
 export type Extension = any;
 export type Service = any;
 export type ExtensionFactory = () => Extension;
