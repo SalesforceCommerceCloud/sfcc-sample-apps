0feab4555beae55ae51de7dda80651fcb335589b
Gurpreet Saini <gurpreet.saini@salesforce.com>
2020-03-25 19:31:23 -0700

    New: Show Product Quantity on Header Cart

--- packages/storefront-lwc/src/modules/commerce/headerBasket/headerBasket.js
+++ packages/storefront-lwc/src/modules/commerce/headerBasket/headerBasket.js
@@ -4,14 +4,30 @@
     SPDX-License-Identifier: BSD-3-Clause
     For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
 */
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import { ShoppingBasket } from 'commerce/data';
+import { useQuery } from '@lwce/apollo-client';
+import gql from 'graphql-tag';
+
+const QUERY = gql`
+    query {
+        getBasketProductCount
+    }
+`;
 
 /**
  * Header basket component that should show up in the header
  */
 export default class HeaderBasket extends LightningElement {
     quantity = 0;
+
+    @wire(useQuery, {
+        query: QUERY,
+        lazy: false,
+    })
+    updateProductsTotal(response) {
+        this.quantity = response.data ? response.data.getBasketProductCount : 0;
+    }
 
     constructor() {
         super();
@@ -26,8 +42,4 @@ export default class HeaderBasket extends LightningElement {
 
     // eslint-disable-next-line class-methods-use-this
     renderedCallback() {}
-
-    connectedCallback() {
-        this.quantity = ShoppingBasket.basket.totalProductsQuantity || 0;
-    }
 }
