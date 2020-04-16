52cf274102aa2367f1358f94d8531602fb6f954d
Bret Little <blittle@salesforce.com>
2020-04-16 16:49:57 -0400

    Fix(storefront-lwc): fix infinite loop possibility on error handling (#202)

--- packages/storefront-lwc/src/modules/commerce/storeFront/storeFront.js
+++ packages/storefront-lwc/src/modules/commerce/storeFront/storeFront.js
@@ -3,6 +3,8 @@ import { history } from '@lwce/router';
 
 export default class StoreFront extends LightningElement {
     query = '';
+    errorCount = 0;
+    lastError = null;
     error = null;
     @wire(history) history;
 
@@ -22,14 +24,27 @@ export default class StoreFront extends LightningElement {
     }
 
     errorCallback(error) {
-        this.error = error;
+        if (
+            (this.lastError
+                ? error.message === this.lastError.message
+                : true) &&
+            this.errorCount < 1
+        ) {
+            console.error(error);
 
-        const unlisten = this.template
-            .querySelector('lwce-router')
-            .getHistory()
-            .listen(() => {
-                this.error = null;
-                unlisten();
-            });
+            this.lastError = this.error;
+            this.error = error;
+            this.errorCount++;
+
+            const unlisten = this.template
+                .querySelector('lwce-router')
+                .getHistory()
+                .listen(() => {
+                    this.errorCount = 0;
+                    this.lastError = null;
+                    this.error = null;
+                    unlisten();
+                });
+        }
     }
 }
